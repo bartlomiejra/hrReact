06380cc77c1af8382411c21b93812250af961a83
Bartłomiej <bartlomiej.ra@gmail.com>
2022-04-19 20:26:45 +0200

    update

---
+++ src/components/data/users.js
@@ -0,0 +1,26 @@
+export const users = [
+  {
+    name: 'Bar Racjhański',
+    attendance: '38%',
+    average: '5,4',
+  },
+  {
+    name: 'Arek Czarkowski',
+    attendance: '11%',
+    average: '2,4',
+  },
+  {
+    name: 'Bary  London',
+    attendance: '38%',
+    average: '3,4',
+  },
+  {
+    name: 'Barroors Racsshański',
+    attendance: '38%',
+    average: '2,2',
+  },
+  {
+    name: 'Alphy Andreas',
+    average: '1,4',
+  },
+];
