ccc3be2a836fff175a4d32f901dc0ce9346aa238
bartlomiejra <bartlomiej.ra@gmail.com>
2022-05-22 20:45:18 +0200

    hooks

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,57 +1,56 @@
-import React from 'react';
-import { users } from 'data/users';
+import React, { useState, useEffect } from 'react';
+import { users as usersData } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { StyledList, Wrapper } from './UsersList.styles';
 
-const mockApi = (success) => {
+const mockAPI = (success) => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (users) {
-        resolve([...users]);
+      if (usersData) {
+        resolve([...usersData]);
       } else {
         reject({ message: 'Error' });
       }
     }, 2000);
   });
 };
-class UsersList extends React.Component {
-  state = {
-    users: [],
-    isLoading: false,
+
+const UsersList = () => {
+  const { users, setUsers } = useState();
+  const [isLoading, setLoadingState] = useState(true);
+
+  useEffect(() => {
+    setLoadingState(true);
+    mockAPI()
+      .then((data) => {
+        setLoadingState(false);
+        setUsers(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    console.log('Loading state');
+  }, [isLoading]);
+
+  const deleteUser = (name) => {
+    const filteredUsers = users.filter((user) => user.name !== name);
+    setUsers(filteredUsers);
   };
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    mockApi().then((data) => {
-      this.setState({ isLoading: false });
-      this.setState({ users: data });
-    });
-  }
+  return (
+    <Wrapper>
+      <h1>{isLoading ? 'Loading' : 'Users List'} </h1>
+      {/* <h1>{this.props.title}</h1> */}
+      {/* <h1>{this.props.isUsersList ? `User's List` : 'Students List'}</h1> */}
+      {/* <button onClick={this.toggleListTitle}>Change title</button> */}
+      <StyledList>
+        {usersData.map((userData) => (
+          <UsersListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
+        ))}
+      </StyledList>
+    </Wrapper>
+  );
+};
 
-  componentDidUpdate(prevState) {
-    if (prevState.isLoading !== this.state.isLoading) {
-      console.log('Loading state has changed');
-    }
-  }
-
-  deleteUser = (name) => {
-    const filteredUsers = this.state.users.filter((user) => user.name !== name);
-    this.setState({ users: filteredUsers });
-  };
-  render() {
-    return (
-      <Wrapper>
-        <h1>{this.state.isLoading ? 'Loading' : 'Users List'} </h1>
-        {/* <h1>{this.props.title}</h1> */}
-        {/* <h1>{this.props.isUsersList ? `User's List` : 'Students List'}</h1> */}
-        <button onClick={this.toggleListTitle}>Change title</button>
-        <StyledList>
-          {this.state.users.map((userData, i) => (
-            <UsersListItem deleteUser={this.deleteUser} index={i} key={userData.name} userData={userData} />
-          ))}
-        </StyledList>
-      </Wrapper>
-    );
-  }
-}
 export default UsersList;
