4b7f06af8458fdbdfb1d0c5212c65813853cc54b
bartlomiejra <bartlomiej.ra@gmail.com>
2022-05-22 19:03:32 +0200

    ASD

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -3,13 +3,33 @@ import { users } from 'data/users';
 import UsersListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { StyledList, Wrapper } from './UsersList.styles';
 
-const UsersList = () => (
-  <Wrapper>
-    <StyledList>
-      {users.map((userData, i) => (
-        <UsersListItem index={i} key={userData.name} userData={userData} />
-      ))}
-    </StyledList>
-  </Wrapper>
-);
+class UsersList extends React.Component {
+  state = {
+    isUsersList: true,
+  };
+
+  toggleListTitle = () => {
+    this.setState((prevState) => ({ isUsersList: prevState.isUsersList }));
+  };
+  render() {
+    //jeśli chcsz dokonać dstrokturyzacji w klasowym komonecie to najlepiej użyć desturkturyzacji wewnatrz funkcji render:
+    const { title } = this.props;
+    // jeśli trzeba coś wyciągnać ze stanu to:
+    const { isUsersList } = this.state;
+    //w ten sposób można wykorzystywać krótszych form w jsx, przykład poniżej w pierwszym h1
+    return (
+      <Wrapper>
+        <h1>{title}</h1>
+        <h1>{this.props.title}</h1>
+        <h1>{this.props.isUsersList ? `User's List` : 'Students List'}</h1>
+        <button onClick={this.toggleListTitle}>Change title</button>
+        <StyledList>
+          {users.map((userData, i) => (
+            <UsersListItem index={i} key={userData.name} userData={userData} />
+          ))}
+        </StyledList>
+      </Wrapper>
+    );
+  }
+}
 export default UsersList;
