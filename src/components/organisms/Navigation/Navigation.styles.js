0c440b9a64aa3124de22d485169653a67d0cd08e
bartlomiejra <bartlomiej.ra@gmail.com>
2022-05-25 22:26:24 +0200

    nav-start

--- src/components/organisms/Navigation/Navigation.styles.js
+++ src/components/organisms/Navigation/Navigation.styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
+import { NavLink } from 'react-router-dom';
 export const Wrapper = styled.nav`
   width: 100%;
   height: 100%;
